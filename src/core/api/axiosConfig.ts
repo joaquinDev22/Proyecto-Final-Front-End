9a8df344b4b7f0df5cd75189c1f3d12531234ca1
joaki <joaking102003@gmail.com>
2026-06-18 23:18:02 -0300

    correccion del link para conexion con API

--- src/core/api/axiosConfig.ts
+++ src/core/api/axiosConfig.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 // const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080';
-const API_URL = import.meta.env.VITE_API_URL || 'proyecto-final-progra-3-production.up.railway.app';
+const API_URL = import.meta.env.VITE_API_URL || 'https://proyecto-final-progra-3-production.up.railway.app';
 
 const api = axios.create({
   baseURL: API_URL,
